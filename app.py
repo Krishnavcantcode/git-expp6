cd5972555f1d937dcbaca60f63b4f58e9a878896
Krishnav <krishnavkns@gmail.com>
2026-03-10 11:31:23 +0530

    AppID Added.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app =Flask(__name__)
 @app.route("/")
 
 def index():
-    return "<h1>Krishnav Sir</h1>"
+    return "<h1>Krishnav Sir</h1><br><h1>AppId =2410807</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
